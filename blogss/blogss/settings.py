3728e60e529ae56dafc42c6e06e703a7da01eb89
atom-jk <1843211035@qq.com>
2024-05-26 13:23:15 +0800

    '数据库配置

--- blogss/blogss/settings.py
+++ blogss/blogss/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'blogss.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'127.0.0.1',
+        'PORT':3306,
+        'USER':'itheima',#用户名
+        'PASSWORD':'123456',
+
+        'NAME': 'itheima_blog',#数据库名字
     }
 }
 
